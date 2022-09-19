fcf50e67f225aa21e802d4fff798dbc3d1009e6d
sleepyash421 <113919558+sleepyash421@users.noreply.github.com>
2022-09-19 21:48:15 +0700

    Add files via upload

---
+++ p.js
@@ -0,0 +1,7 @@
+if (screen.width < 500 ||
+ navigator.userAgent.match(/Android/i) ||
+ navigator.userAgent.match(/webOS/i) ||
+ navigator.userAgent.match(/iPhone/i) ||
+ navigator.userAgent.match(/iPod/i)){	 
+	(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'//pl17723810.profitablegatetocontent.com/37/42/04/374204434b35164cfebad5298ec9f7c4.js',document.body||document.documentElement);
+	 }
