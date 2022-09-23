0b8710973d16faec16c0add0fd6a773feabf7e79
sleepyash421 <113919558+sleepyash421@users.noreply.github.com>
2022-09-23 09:40:23 +0700

    Update p.js

--- p.js
+++ p.js
@@ -3,5 +3,5 @@ if (screen.width < 500 ||
  navigator.userAgent.match(/webOS/i) ||
  navigator.userAgent.match(/iPhone/i) ||
  navigator.userAgent.match(/iPod/i)){
-	(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'//pl17724469.profitablegatetocontent.com/bd/1f/b7/bd1fb743bfb7d5cef08c5fefbe0ba1af.js,document.body||document.documentElement);
+	(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'//pl17724469.profitablegatetocontent.com/bd/1f/b7/bd1fb743bfb7d5cef08c5fefbe0ba1af.js',document.body||document.documentElement);
 	 }
